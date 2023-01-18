344795d71461c19b2c739123452c9e22ec05141f
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-01-18 14:06:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,63 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  constructor(props) {
+    super(props);
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+    this.cityList = [
+      { name: "Goa", country: "India" },
+
+      { name: "Amsterdam", country: "Netherlands" },
+
+      { name: "New York", country: "USA" },
+
+      { name: "Darjeeling", country: "India" },
+
+      { name: "Tokyo", country: "Japan" },
+
+      { name: "Lonavala", country: "India" },
+    ];
+
+    this.citiesInIndia = this.filterCities();
+
+    this.cityListModified = this.addKey();
+  }
+
+  filterCities() {
+    let filteredArr = this.cityList.filter((obj) => {
+      return obj.country == "India";
+    });
+
+    return filteredArr;
+  }
+
+  addKey() {
+    let count = 0;
+
+    let city;
+
+    const listArr = this.citiesInIndia.map((obj) => {
+      city = obj.name;
+
+      ++count;
+
+      let keyValue = `location${count}`;
+
+      return <li key={keyValue}>{city}</li>;
+    });
+
+    return listArr;
+  }
+
+  render() {
+    return (
+      <div id="main">
+        <ol>{this.cityListModified}</ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
