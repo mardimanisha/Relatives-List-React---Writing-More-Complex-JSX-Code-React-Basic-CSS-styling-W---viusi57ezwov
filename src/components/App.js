45b1d98a1e8ec6fc72aed7ca84367f9479fab29a
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-01-19 00:10:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,63 +1,30 @@
 import React, { Component, useState } from "react";
-
-import "../styles/App.css";
+import '../styles/App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.cityList = [
-      { name: "Goa", country: "India" },
-
-      { name: "Amsterdam", country: "Netherlands" },
-
-      { name: "New York", country: "USA" },
-
-      { name: "Darjeeling", country: "India" },
-
-      { name: "Tokyo", country: "Japan" },
-
-      { name: "Lonavala", country: "India" },
-    ];
-
-    this.citiesInIndia = this.filterCities();
-
-    this.cityListModified = this.addKey();
-  }
-
-  filterCities() {
-    let filteredArr = this.cityList.filter((obj) => {
-      return obj.country == "India";
-    });
-
-    return filteredArr;
-  }
-
-  addKey() {
-    let count = 0;
-
-    let city;
-
-    const listArr = this.citiesInIndia.map((obj) => {
-      city = obj.name;
-
-      ++count;
-
-      let keyValue = `location${count}`;
-
-      return <li key={keyValue}>{city}</li>;
-    });
-
-    return listArr;
-  }
-
-  render() {
-    return (
-      <div id="main">
-        <ol>{this.cityListModified}</ol>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            relatives: [
+                { key: 'relativeListItem1', name: 'Mom' },
+                { key: 'relativeListItem2', name: 'Dad' },
+                { key: 'relativeListItem3', name: 'Sister' },
+                { key: 'relativeListItem4', name: 'Brother' },
+            ],
+        };
+    }
+    render() {
+        return (
+            <div id="main">
+                <h3>Diwali Shopping List</h3>
+                <ol key="relativeList">
+                    {this.state.relatives.map(relative => (
+                        <li key={relative.key}>{relative.name}</li>
+                    ))}
+                </ol>
+            </div>
+        )
+    }
 }
 
 export default App;
